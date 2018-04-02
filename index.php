2b45812d10b5e720827d49e01e0ad60ab38b6c11
Javier Aliaga <javiealiaga@gmail.com>
2018-04-02 20:54:08 +0200

    bloginfo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="<<?php bloginfo(stylesheet_url) ?>">
+</head>
+<body>
+	<h1><?php bloginfo('name') ?></h1>
+	<h2><?php bloginfo( 'descripction' ) ?></h2>
+	<ul>
+		<li><?php bloginfo( 'name') ;?></li>
+		<li><?php bloginfo( 'description') ;?></li>
+		<li><?php bloginfo( 'wpurl') ;?></li>
+		<li><?php bloginfo( 'admin_email') ;?></li>
+		<li><?php bloginfo( 'charset') ;?></li>
+		<li><?php bloginfo( 'html_type') ;?></li>
+		<li><?php bloginfo( 'version') ;?></li>
+		<li><?php bloginfo( 'stylesheet_url') ;?></li>
+		<li><?php bloginfo( 'stylesheet_directory') ;?></li>
+		
+	</ul>
+</body>
+</html>
