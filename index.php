4e5bc5511e1ac0082c42832075f8d085b91b34e5
Javier Aliaga <javiealiaga@gmail.com>
2018-04-02 22:25:01 +0200

    structure html

--- index.php
+++ index.php
@@ -3,22 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="<<?php bloginfo(stylesheet_url) ?>">
+	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url) ?>">
 </head>
 <body>
-	<h1><?php bloginfo('name') ?></h1>
-	<h2><?php bloginfo( 'descripction' ) ?></h2>
-	<ul>
-		<li><?php bloginfo( 'name') ;?></li>
-		<li><?php bloginfo( 'description') ;?></li>
-		<li><?php bloginfo( 'wpurl') ;?></li>
-		<li><?php bloginfo( 'admin_email') ;?></li>
-		<li><?php bloginfo( 'charset') ;?></li>
-		<li><?php bloginfo( 'html_type') ;?></li>
-		<li><?php bloginfo( 'version') ;?></li>
-		<li><?php bloginfo( 'stylesheet_url') ;?></li>
-		<li><?php bloginfo( 'stylesheet_directory') ;?></li>
-		
-	</ul>
+	<header>
+		<h1><?php bloginfo('name') ?></h1>
+		<h2><?php bloginfo( 'descripction' ) ?></h2>
+	</header>
+	<section class="content">
+		<article>
+			<header>
+				<h3>titulo</h3>
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae molestiae rem ipsa ab perferendis obcaecati eligendi facilis ex possimus asperiores tempora totam tenetur accusantium reiciendis sequi sint, facere, quidem, esse.</p>
+		</article>
+		<article>
+			<header>
+				<h3>titulo</h3>
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem a odio, similique inventore molestias voluptas esse possimus, ipsum libero illo accusamus, nostrum nam natus sequi magnam perferendis tempora, facere.</p>
+		</article>
+		<article>
+			<header>
+				<h3>titulo</h3>
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto saepe atque quod ullam tempora sit aliquid et natus ab assumenda! Animi tenetur, blanditiis fuga doloremque voluptates quo! Fuga, suscipit, ipsa?</p>
+		</article>
+	</section>
 </body>
 </html>
